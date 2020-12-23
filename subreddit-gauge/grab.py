2e9f8d68ae7d8b1656cf6402cb4916357c5798c6
maybemaby <74226064+maybemaby@users.noreply.github.com>
2020-12-22 21:21:01 -0800

    Return mult. subreddit subcounts and active users.

--- subreddit-gauge/grab.py
+++ subreddit-gauge/grab.py
@@ -2,8 +2,7 @@
 # Uses praw to pull data about subreddits.
 
 import praw
-import numpy as np
-from secrets import client_id, client_secret, reddit_password
+from credentials import client_id, client_secret, reddit_password
 
 # praw object
 reddit = praw.Reddit(
@@ -15,35 +14,51 @@ reddit = praw.Reddit(
 )
 
 
-def get_subscribers(subreddit_):
-    """Gets current sub count.
+def get_subscribers(subreddit_, *args):
+    """Gets current sub count for one or more subreddits.
 
     Inputs
     -------
-    str: Desired subreddit name
+    str: Desired subreddit name(s)
 
     Returns
     -------
-    int: sub count
+    int: sub count or dict:{subreddit: int(sub count)}
     """
-    subreddit = reddit.subreddit(subreddit_)
-    return subreddit.subscribers()
+    if len(args) > 0:
+        subreddit = reddit.subreddit(subreddit_)
+        subcount = {subreddit_: subreddit.subscribers}
+        for page in args:
+            subreddit = reddit.subreddit(page)
+            subcount[page] = subreddit.subscribers
+        return subcount
+    else:
+        subreddit = reddit.subreddit(subreddit_)
+        return subreddit.subscribers
 
 
-def get_active(subreddit_):
+def get_active(subreddit_, *args):
     """Gets amount of users actively on subreddit.
 
     Inputs
     -------
-    str: Desired subreddit name
+    str: Desired subreddit name(s)
 
     Returns
     -------
-    int: Currently online user count.
+    int: Currently online user count or dict:{subreddit: int(active user count)}
     """
-    subreddit = reddit.subreddit(subreddit_)
-    subreddit._fetch()
-    return subreddit.active_user_count
+    if len(args) > 0:
+        subreddit = reddit.subreddit(subreddit_)
+        usercounts = {subreddit_: subreddit.subscribers}
+        for page in args:
+            subreddit = reddit.subreddit(page)
+            usercounts[page] = subreddit.active_user_count
+        return usercounts
+    else:
+        subreddit = reddit.subreddit(subreddit_)
+        subreddit._fetch()
+        return subreddit.active_user_count
 
 
 def get_posts(subreddit_):
@@ -62,9 +77,3 @@ def get_posts(subreddit_):
         score = submission.score
         posts[submission.id] = [score, len(submission.comments.list())]
     return posts
-
-
-data = get_posts("learnpython")
-print(data)
-print(len(data))
-print(get_active("learnpython"))

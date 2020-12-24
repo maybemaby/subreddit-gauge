3f690909379a6e12f2a2a30c68910d3deb73c1eb
maybemaby <74226064+maybemaby@users.noreply.github.com>
2020-12-23 17:23:42 -0800

    Added comments/hour to the dataframe.

--- subreddit-gauge/main.py
+++ subreddit-gauge/main.py
@@ -16,11 +16,13 @@ def main():
     timeindex = []
     data = []
     postdata = {}
-    for _ in range(24):
+    for _ in range(1):
         hourly_posts = get_posts("learnpython")
         upvotes = 0
+        comments = 0
         for v in hourly_posts.values():
             upvotes += v[0]
+            comments += v[1]
         t = datetime.datetime.now()
         timeindex.append(t.strftime("%c"))
         data.append(
@@ -29,19 +31,26 @@ def main():
                 get_active("learnpython"),
                 len(hourly_posts),
                 upvotes,
+                comments,
             ]
         )
-        if postdata == None:
+        if postdata == {}:
             postdata = hourly_posts
         else:
             postdata = postdata.update(hourly_posts)
-        time.sleep(10)
+        # time.sleep(10)
     array1 = np.array(data)
     dti = pd.DatetimeIndex(timeindex)
     df = pd.DataFrame(
         array1,
         index=dti,
-        columns=["Subs", "Active users", "Posts/hour", "Post upvotes/hour"],
+        columns=[
+            "Subs",
+            "Active users",
+            "Posts/hour",
+            "Post upvotes/hour",
+            "Comments/hour",
+        ],
     )
     df2 = pd.DataFrame.from_dict(
         postdata, orient="index", columns=["Scores", "Number of Comments"]
@@ -49,11 +58,14 @@ def main():
     return df, df2
 
 
-subreddit_data = main()
-for i, df in enumerate(subreddit_data):
-    filename = "learnpythondata" + str(i) + ".csv"
-    existing_file = Path(path + filename)
-    if existing_file.is_file():
-        df.to_csv(filename, mode="a", header=False)
-    else:
-        df.to_csv(path_or_buf=path + filename)
+if __name__ == "__main__":
+    subreddit_data = main()
+    for i, df in enumerate(subreddit_data):
+        filename = "learnpythondata" + str(i) + ".csv"
+        existing_file = Path(path + filename)
+        if existing_file.is_file():
+            print(f"Adding to {filename}...")
+            df.to_csv(path + filename, mode="a", header=False)
+        else:
+            df.to_csv(path_or_buf=path + filename)
+            print(f"Creating {filename}...")
